9c0f6787328afa2e7f26e9bf724eebdd77277fc0
sbretall <47549144+sbretall@users.noreply.github.com>
2019-06-01 10:50:14 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,18 +3,20 @@ var imgCount = 6;
 var dir = 'images/index/';
 var randomCount = Math.round(Math.random() * (imgCount - 1)) + 1;
 var images = new Array
-        images[1] = "1.jpg",
-        images[2] = "2.jpg",
-        images[3] = "3.jpg",
-        images[4] = "4.jpg",
-        images[5] = "5.jpg",
-        images[6] = "6.jpg",
-        document.getElementById("GalleryPreview").style.backgroundImage = "url(" + dir + images[randomCount] + ")";
+        images[1] = "gallery1",
+        images[2] = "gallery2",
+        images[3] = "gallery3",
+        images[4] = "gallery4",
+        images[5] = "gallery5",
+        images[6] = "gallery6",
+        document.getElementById(images[randomCount]).style.display = "block";
 var timer = setInterval(() => {
     if (images[randomCount] < images[6]){
+        document.getElementById(images[randomCount]).style.display = "none";
         images[randomCount  += 1]
     } else {
+        document.getElementById(images[randomCount]).style.display = "none";
         randomCount = 1
     }
-    document.querySelector("#GalleryPreview").style.backgroundImage = "url(" + dir + images[randomCount] + ")";
+    document.getElementById(images[randomCount]).style.display = "block";
 }, 5000);
